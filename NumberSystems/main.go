644edc769ed5d2cba04e2eb8551870532ce53e5b
gnosthi <kenneth@yaay,us>
2017-06-27 09:29:37 -0500

    Writing stuff!

---
+++ NumberSystems/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 666
+	b := "Do what thou wilt shall be the whole of the Law"
+	c := 93
+	d := "Love is the law, love under will."
+
+	fmt.Printf("Decimal: %d, Binary: %b, Hex: %x\n", a, a, a)
+	fmt.Printf("String: %s, \n Hex: %x\n",b, b)
+	fmt.Printf("Decimal: %d, Binary: %b, Hex: %x\n", c, c, c)
+	fmt.Printf("String: %s, \n Hex: %x\n", d, d)
+}
