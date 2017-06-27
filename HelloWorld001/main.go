21c39570b4806bf3fcda0a645638c34d7532e195
gnosthi <kenneth@yaay,us>
2017-06-27 00:00:08 -0500

    Writing stuff!

--- HelloWorld001/main.go
+++ HelloWorld001/main.go
@@ -2,7 +2,20 @@ package main
 
 import "fmt"
 
+func testBones(x int, y int) int {
+	return x * y
+}
+
 func main() {
 	x := "Hello, World"
+	a := 3
+	b := 4
+	y := testBones(a, b)
 	fmt.Println(x)
+
+	if y == 4 {
+		fmt.Println("y = 10")
+	} else {
+		fmt.Println("y =", y)
+	}
 }
